4dc056ae24443cbbd4e6056268dddb97b42f11b6
MgKyaw <mgkyaw20112@gmail.com>
2025-02-13 20:00:39 +0700

    feat: Add new method DisplayRandomNumbers

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿void DisplayRandomNumbers()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"{random.Next(1, 100)} ");
+    }
+
+    Console.WriteLine();
+}
