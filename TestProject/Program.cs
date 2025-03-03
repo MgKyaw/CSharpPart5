3a7fc26a17e14d0e14dfea40251d2113793fb8c6
MgKyaw <mgkyaw20112@gmail.com>
2025-03-03 23:50:03 +0700

    feat: Add code for method scope

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -201,3 +201,17 @@
 // }
 
 // DisplayAdjustedTimes(schedule, 6, -6);
+
+string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
+
+DisplayStudents(students);
+DisplayStudents(new string[] {"Robert","Vanya"});
+
+void DisplayStudents(string[] students) 
+{
+    foreach (string student in students) 
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
