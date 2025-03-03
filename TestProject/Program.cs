d2523d07fa2a38d78102113e79791470c6ba3c74
MgKyaw <mgkyaw20112@gmail.com>
2025-03-03 23:46:26 +0700

    feat: Add code for DisplayAdjustedTimes

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -179,5 +179,9 @@ int[] schedule = {800, 1200, 1600, 2000};
 
 void DisplayAdjustedTimes(int[] times, int currentGMT, int newGMT) 
 {
-
+    int diff = 0;
+    if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+    {
+        Console.WriteLine("Invalid GMT");
+    }
 }
