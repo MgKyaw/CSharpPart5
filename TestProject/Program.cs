06b8745e513f41ef1fb4e4a7d7362fdcb3b033c9
MgKyaw <mgkyaw20112@gmail.com>
2025-03-04 23:19:57 +0700

    feat: Comment code for parms

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -242,15 +242,15 @@
 //     Console.WriteLine($"Circumference = {circumference}");
 // }
 
-int a = 3;
-int b = 4;
-int c = 0;
+// int a = 3;
+// int b = 4;
+// int c = 0;
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+// Multiply(a, b, c);
+// Console.WriteLine($"global statement: {a} x {b} = {c}");
 
-void Multiply(int a, int b, int c) 
-{
-    c = a * b;
-    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
-}
+// void Multiply(int a, int b, int c) 
+// {
+//     c = a * b;
+//     Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+// }
