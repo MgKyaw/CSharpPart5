2a668a785fa10a68cf7e0ba424685a812bd1b585
MgKyaw <mgkyaw20112@gmail.com>
2025-03-04 23:27:45 +0700

    feat: Search guest list

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -298,7 +298,19 @@ void RSVP(string name, int partySize, string allergies, bool inviteOnly)
 {
     if (inviteOnly)
     {
-        // search guestList before adding rsvp
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name)) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
     }
 
     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
