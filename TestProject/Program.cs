97ae5c5ed4125a1837cd1e8f053d5f8714b99403
MgKyaw <mgkyaw20112@gmail.com>
2025-03-04 23:25:31 +0700

    feat: Comment code for string

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -278,14 +278,14 @@
 //     }
 // }
 
-string status = "Healthy";
+// string status = "Healthy";
 
-Console.WriteLine($"Start: {status}");
-SetHealth(false);
-Console.WriteLine($"End: {status}");
+// Console.WriteLine($"Start: {status}");
+// SetHealth(false);
+// Console.WriteLine($"End: {status}");
 
-void SetHealth(bool isHealthy) 
-{
-    status = (isHealthy ? "Healthy" : "Unhealthy");
-    Console.WriteLine($"Middle: {status}");
-}
+// void SetHealth(bool isHealthy) 
+// {
+//     status = (isHealthy ? "Healthy" : "Unhealthy");
+//     Console.WriteLine($"Middle: {status}");
+// }
