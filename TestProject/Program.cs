f922f04908c2d26974ab764ed98eecb3e40b3a13
MgKyaw <mgkyaw20112@gmail.com>
2025-03-04 23:11:12 +0700

    feat: Add PrintCircleArea

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -215,3 +215,12 @@
 //     }
 //     Console.WriteLine();
 // }
+
+PrintCircleArea(12);
+
+void PrintCircleArea(int radius)
+{
+    double pi = 3.14159;
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
