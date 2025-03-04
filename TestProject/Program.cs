639d44a4a577822c8335b3d624ded7fe29bb8c43
MgKyaw <mgkyaw20112@gmail.com>
2025-03-04 23:18:57 +0700

    feat: Add code for parms

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -241,3 +241,16 @@
 //     double circumference = 2 * pi * radius;
 //     Console.WriteLine($"Circumference = {circumference}");
 // }
+
+int a = 3;
+int b = 4;
+int c = 0;
+
+Multiply(a, b, c);
+Console.WriteLine($"global statement: {a} x {b} = {c}");
+
+void Multiply(int a, int b, int c) 
+{
+    c = a * b;
+    Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+}
