3b71e8c0fc75d1572a41d438819dcd5680105e82
MgKyaw <mgkyaw20112@gmail.com>
2025-03-04 23:21:49 +0700

    feat: Add code for strings

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -277,3 +277,15 @@
 //         array[i] = 0;
 //     }
 // }
+
+string status = "Healthy";
+
+Console.WriteLine($"Start: {status}");
+SetHealth(status, false);
+Console.WriteLine($"End: {status}");
+
+void SetHealth(string status, bool isHealthy) 
+{
+    status = (isHealthy ? "Healthy" : "Unhealthy");
+    Console.WriteLine($"Middle: {status}");
+}
