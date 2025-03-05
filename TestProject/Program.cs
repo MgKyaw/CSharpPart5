c08ed6d86b81522fe0ef14af003b4b87aa794925
MgKyaw <mgkyaw20112@gmail.com>
2025-03-05 21:06:32 +0700

    feat: Add the beginning of code challenge

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -333,3 +333,28 @@
 // RSVP("Noor", 4, inviteOnly: false);
 // RSVP("Jonte", 2, "Stone fruit", false);
 // ShowRSVPs();
+
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    // display internal email addresses
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    // display external email addresses
+}
