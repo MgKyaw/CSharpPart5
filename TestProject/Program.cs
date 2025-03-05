f2996dd395afbde84ed77a23e1ca1376a8d8c114
MgKyaw <mgkyaw20112@gmail.com>
2025-03-05 21:33:31 +0700

    feat: Comment code challenge

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -334,50 +334,50 @@
 // RSVP("Jonte", 2, "Stone fruit", false);
 // ShowRSVPs();
 
-string[,] corporate = 
-{
-    {"Robert", "Bavin"}, {"Simon", "Bright"},
-    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
-    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
-};
-
-string[,] external = 
-{
-    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
-    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
-};
-
-string externalDomain = "hayworth.com";
-
-// for (int i = 0; i < corporate.GetLength(0); i++) 
+// string[,] corporate = 
 // {
-//     DisplayEmailAddress(firstName: corporate[i, 0], lastName: corporate[i, 1]);
-// }
+//     {"Robert", "Bavin"}, {"Simon", "Bright"},
+//     {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+//     {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+// };
 
-// for (int i = 0; i < external.GetLength(0); i++) 
+// string[,] external = 
 // {
-//     DisplayEmailAddress(firstName: external[i, 0], lastName: external[i, 1], externalDomain);
-// }
+//     {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+//     {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+// };
 
-// void DisplayEmailAddress(string firstName, string lastName, string domainName = "contoso.com")
-// {
-//     string firstTwo = firstName.Length >= 2 ? firstName.Substring(0, 2) : firstName;
-//     System.Console.WriteLine($"{firstTwo?.ToLower()}{lastName.ToLower()}@{domainName}");
-// }
+// string externalDomain = "hayworth.com";
 
-	for (int i = 0; i < corporate.GetLength(0); i++) 
-	{
-		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
-	}
+// // for (int i = 0; i < corporate.GetLength(0); i++) 
+// // {
+// //     DisplayEmailAddress(firstName: corporate[i, 0], lastName: corporate[i, 1]);
+// // }
 
-	for (int i = 0; i < external.GetLength(0); i++) 
-	{
-		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
-	}
+// // for (int i = 0; i < external.GetLength(0); i++) 
+// // {
+// //     DisplayEmailAddress(firstName: external[i, 0], lastName: external[i, 1], externalDomain);
+// // }
 
-	void DisplayEmail(string first, string last, string domain = "contoso.com") 
-	{
-		string email = first.Substring(0, 2) + last;
-		email = email.ToLower();
-		Console.WriteLine($"{email}@{domain}");
-	}
+// // void DisplayEmailAddress(string firstName, string lastName, string domainName = "contoso.com")
+// // {
+// //     string firstTwo = firstName.Length >= 2 ? firstName.Substring(0, 2) : firstName;
+// //     System.Console.WriteLine($"{firstTwo?.ToLower()}{lastName.ToLower()}@{domainName}");
+// // }
+
+// 	for (int i = 0; i < corporate.GetLength(0); i++) 
+// 	{
+// 		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+// 	}
+
+// 	for (int i = 0; i < external.GetLength(0); i++) 
+// 	{
+// 		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+// 	}
+
+// 	void DisplayEmail(string first, string last, string domain = "contoso.com") 
+// 	{
+// 		string email = first.Substring(0, 2) + last;
+// 		email = email.ToLower();
+// 		Console.WriteLine($"{email}@{domain}");
+// 	}
