3eb9f5346e3164ce2f132d12d8130f4bab34eafd
MgKyaw <mgkyaw20112@gmail.com>
2025-03-05 21:31:02 +0700

    feat: Add solution code

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -364,3 +364,20 @@ string externalDomain = "hayworth.com";
 //     string firstTwo = firstName.Length >= 2 ? firstName.Substring(0, 2) : firstName;
 //     System.Console.WriteLine($"{firstTwo?.ToLower()}{lastName.ToLower()}@{domainName}");
 // }
+
+	for (int i = 0; i < corporate.GetLength(0); i++) 
+	{
+		DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
+	}
+
+	for (int i = 0; i < external.GetLength(0); i++) 
+	{
+		DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+	}
+
+	void DisplayEmail(string first, string last, string domain = "contoso.com") 
+	{
+		string email = first.Substring(0, 2) + last;
+		email = email.ToLower();
+		Console.WriteLine($"{email}@{domain}");
+	}
