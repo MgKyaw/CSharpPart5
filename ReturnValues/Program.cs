db3841f907d11e28a5d35ff2263e1124bd0b6ce6
MgKyaw <mgkyaw20112@gmail.com>
2025-03-06 22:37:46 +0700

    feat: Call VndToUsd method

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -37,6 +37,7 @@ double usd = 23.73;
 int vnd = UsdToVnd(usd);
 
 Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
 
 int UsdToVnd(double usd) 
 {
