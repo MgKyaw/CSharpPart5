c57d7cc521d55f677390c555e5a779b660fddcc5
MgKyaw <mgkyaw20112@gmail.com>
2025-03-06 22:38:55 +0700

    feat: Comment Unit 3

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -31,22 +31,22 @@
 //     return input.ToString().Substring(0, 5);
 // }
 
-// Unit 3
+// // Unit 3
 
-double usd = 23.73;
-int vnd = UsdToVnd(usd);
+// double usd = 23.73;
+// int vnd = UsdToVnd(usd);
 
-Console.WriteLine($"${usd} USD = ${vnd} VND");
-Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+// Console.WriteLine($"${usd} USD = ${vnd} VND");
+// Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
 
-int UsdToVnd(double usd) 
-{
-    int rate = 23500;
-    return (int) (rate * usd);
-}
+// int UsdToVnd(double usd) 
+// {
+//     int rate = 23500;
+//     return (int) (rate * usd);
+// }
 
-double VndToUsd(int vnd) 
-{
-    double rate = 23500;
-    return vnd / rate;
-}
+// double VndToUsd(int vnd) 
+// {
+//     double rate = 23500;
+//     return vnd / rate;
+// }
