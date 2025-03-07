21f99b4be30cb51c3780fad47ef746746c01be00
MgKyaw <mgkyaw20112@gmail.com>
2025-03-07 21:29:12 +0700

    feat: Update TwoCoins method

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -113,5 +113,13 @@
 
 int[] TwoCoins(int[] coins, int target) 
 {
+    for (int curr = 0; curr < coins.Length; curr++) 
+    {
+        for (int next = curr + 1; next < coins.Length; next++) 
+        {
+
+        }
+    }
+
     return  new int[0];
 }
