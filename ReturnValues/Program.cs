d980fe9d85d9b261e2965493876d04f50ef37180
MgKyaw <mgkyaw20112@gmail.com>
2025-03-07 21:16:03 +0700

    feat: Modify IsPalindrome method

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -93,5 +93,18 @@ foreach (string word in words)
 
 bool IsPalindrome(string word) 
 {
+    int start = 0;
+    int end = word.Length - 1;
+
+    while (start < end) 
+    {
+        if (word[start] != word[end]) 
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
     return true;
 }
