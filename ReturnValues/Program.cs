8dbb8f90f2ae21bebd20c360a070d873acaad7cd
MgKyaw <mgkyaw20112@gmail.com>
2025-03-07 21:15:07 +0700

    feat: Add code for Unit 5

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -80,3 +80,13 @@
 
 //     return result.Trim();
 // }
+
+// Unit 5
+
+string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
+
+Console.WriteLine("Is it a palindrome?");
+foreach (string word in words) 
+{
+    Console.WriteLine($"{word}: {IsPalindrome(word)}");
+}
