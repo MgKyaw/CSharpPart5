98f4b33376444042da8fb248c4fcd3beb7d445cc
MgKyaw <mgkyaw20112@gmail.com>
2025-03-07 21:27:59 +0700

    feat: Comment Unit 5

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -81,30 +81,30 @@
 //     return result.Trim();
 // }
 
-// Unit 5
+// // Unit 5
 
-string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
+// string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
 
-Console.WriteLine("Is it a palindrome?");
-foreach (string word in words) 
-{
-    Console.WriteLine($"{word}: {IsPalindrome(word)}");
-}
+// Console.WriteLine("Is it a palindrome?");
+// foreach (string word in words) 
+// {
+//     Console.WriteLine($"{word}: {IsPalindrome(word)}");
+// }
 
-bool IsPalindrome(string word) 
-{
-    int start = 0;
-    int end = word.Length - 1;
+// bool IsPalindrome(string word) 
+// {
+//     int start = 0;
+//     int end = word.Length - 1;
 
-    while (start < end) 
-    {
-        if (word[start] != word[end]) 
-        {
-            return false;
-        }
-        start++;
-        end--;
-    }
+//     while (start < end) 
+//     {
+//         if (word[start] != word[end]) 
+//         {
+//             return false;
+//         }
+//         start++;
+//         end--;
+//     }
 
-    return true;
-}
+//     return true;
+// }
