d628049d9414d1e179143ecf9add5f0b7e99d783
MgKyaw <mgkyaw20112@gmail.com>
2025-03-07 22:11:49 +0700

    feat: Check count with array length

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -124,19 +124,26 @@ else
     Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
 }
 
-int[] TwoCoins(int[] coins, int target) 
+int[,] TwoCoins(int[] coins, int target) 
 {
+    int[,] result = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    int count = 0;
+
     for (int curr = 0; curr < coins.Length; curr++) 
     {
         for (int next = curr + 1; next < coins.Length; next++) 
         {
             if (coins[curr] + coins[next] == target) 
             {
-                return new int[]{curr, next};
+                result[count, 0] = curr;
+                result[count, 1] = next;
+                count++;
             }
 
+            if (count == result.GetLength(0)) 
+            {
+                return result;
+            }
         }
     }
-
-    return  new int[0];
 }
