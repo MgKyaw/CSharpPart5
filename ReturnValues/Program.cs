d7a9767b05e28a9ff2801544e8dea38dd59f906e
MgKyaw <mgkyaw20112@gmail.com>
2025-03-08 22:36:19 +0700

    feat: Comment Solution code for Unit 7

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -201,52 +201,52 @@
 //     return "You lose!";
 // }
 
-Random random = new Random();
+// Random random = new Random();
 
-Console.WriteLine("Would you like to play? (Y/N)");
-if (ShouldPlay()) 
-{
-    PlayGame();
-}
+// Console.WriteLine("Would you like to play? (Y/N)");
+// if (ShouldPlay()) 
+// {
+//     PlayGame();
+// }
 
-bool ShouldPlay() 
-{
-    string response = Console.ReadLine();
-    return response.ToLower().Equals("y");
-}
+// bool ShouldPlay() 
+// {
+//     string response = Console.ReadLine();
+//     return response.ToLower().Equals("y");
+// }
 
-void PlayGame() 
-{
-    var play = true;
+// void PlayGame() 
+// {
+//     var play = true;
 
-    while (play) {
-        var target = GetTarget();
-        var roll = RollDice();
+//     while (play) {
+//         var target = GetTarget();
+//         var roll = RollDice();
 
-        Console.WriteLine($"Roll a number greater than {target} to win!");
-        Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose(roll, target));
-        Console.WriteLine("\nPlay again? (Y/N)");
+//         Console.WriteLine($"Roll a number greater than {target} to win!");
+//         Console.WriteLine($"You rolled a {roll}");
+//         Console.WriteLine(WinOrLose(roll, target));
+//         Console.WriteLine("\nPlay again? (Y/N)");
 
-        play = ShouldPlay();
-    }
-}
+//         play = ShouldPlay();
+//     }
+// }
 
-int GetTarget() 
-{
-    return random.Next(1, 6);
-}
+// int GetTarget() 
+// {
+//     return random.Next(1, 6);
+// }
 
-int RollDice() 
-{
-    return random.Next(1, 7);
-}
+// int RollDice() 
+// {
+//     return random.Next(1, 7);
+// }
 
-string WinOrLose(int roll, int target) 
-{
-    if (roll > target) 
-    {
-        return "You win!";
-    }
-    return "You lose!";
-}
+// string WinOrLose(int roll, int target) 
+// {
+//     if (roll > target) 
+//     {
+//         return "You win!";
+//     }
+//     return "You lose!";
+// }
