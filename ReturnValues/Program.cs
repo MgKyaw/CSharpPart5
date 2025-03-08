18d264486b549a63c183929ec7ddcd85476c0dee
MgKyaw <mgkyaw20112@gmail.com>
2025-03-08 22:05:33 +0700

    feat: Add Code for Unit 7

--- ReturnValues/Program.cs
+++ ReturnValues/Program.cs
@@ -156,3 +156,32 @@
 //     }
 //     return (count == 0) ? new int[0,0] : result;
 // }
+
+// Unit 7
+
+Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay()) 
+{
+    PlayGame();
+}
+
+void PlayGame() 
+{
+    var play = true;
+
+    while (play) 
+    {
+        var target;
+        var roll;
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose());
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
+
